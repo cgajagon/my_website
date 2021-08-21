66192cda96818d36e391736524a058dfd3da979f
CarlosGajard <cgajagon@gmail.com>
2021-08-21 15:21:18 -0400

    deploy

---
+++ _nuxt/static/1629573618/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fmy_website\u002F_nuxt\u002Fstatic\u002F1629573618",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fmy_website\u002F",assetsPath:"\u002Fmy_website\u002F_nuxt\u002F",cdnURL:a}}}}(null));
